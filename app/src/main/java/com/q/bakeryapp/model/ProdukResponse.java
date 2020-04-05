3492039eff70342da8f31019b80c2d7fc3ab6f2f
Qoolqas <qoolqas@gmail.com>
2020-04-05 16:14:47 +0700

    get data

--- app/src/main/java/com/q/bakeryapp/model/ProdukResponse.java
+++ app/src/main/java/com/q/bakeryapp/model/ProdukResponse.java
@@ -5,7 +5,7 @@ import com.google.gson.annotations.SerializedName;
 import java.util.List;
 
 public class ProdukResponse {
-    @SerializedName("data")
+    @SerializedName("result")
     private List<ProdukModel> data;
 
     public List<ProdukModel> getData() {
