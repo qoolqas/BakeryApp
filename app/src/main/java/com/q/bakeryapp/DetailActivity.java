0f59a348dbc3ca96562ff8a59f4605eec7ad5db2
Qoolqas <qoolqas@gmail.com>
2020-04-05 21:18:27 +0700

    detailActivity

--- app/src/main/java/com/q/bakeryapp/DetailActivity.java
+++ app/src/main/java/com/q/bakeryapp/DetailActivity.java
@@ -4,11 +4,15 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 
+import com.q.bakeryapp.model.ProdukModel;
+
 public class DetailActivity extends AppCompatActivity {
+    ProdukModel produkModel;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_detail);
+        produkModel = getIntent().getParcelableExtra("data");
     }
 }
