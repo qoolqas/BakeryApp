75f24acaecab450b87bdc942e3d18cbb2c29fd69
Qoolqas <qoolqas@gmail.com>
2020-06-06 18:57:02 +0700

    pretty much layout (2) and clickable content

--- app/src/main/java/com/q/bakeryapp/ui/home/HomeFragment.java
+++ app/src/main/java/com/q/bakeryapp/ui/home/HomeFragment.java
@@ -1,5 +1,6 @@
 package com.q.bakeryapp.ui.home;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
@@ -8,14 +9,17 @@ import android.widget.TextView;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
+import androidx.cardview.widget.CardView;
 import androidx.fragment.app.Fragment;
 import androidx.lifecycle.Observer;
 import androidx.lifecycle.ViewModelProviders;
+import androidx.navigation.Navigation;
 
 import com.q.bakeryapp.R;
+import com.q.bakeryapp.ui.manage.ManageActivity;
 
 public class HomeFragment extends Fragment {
-
+    CardView cardProduk, cardManage, cardToko;
 
     public View onCreateView(@NonNull LayoutInflater inflater,
                              ViewGroup container, Bundle savedInstanceState) {
@@ -23,5 +27,24 @@ public class HomeFragment extends Fragment {
         View root = inflater.inflate(R.layout.fragment_home, container, false);
 
         return root;
+    }
+
+    @Override
+    public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+        cardProduk = view.findViewById(R.id.cardProduk);
+        cardManage = view.findViewById(R.id.cardManage);
+        cardToko = view.findViewById(R.id.cardToko);
+        cardProduk.setOnClickListener(Navigation.createNavigateOnClickListener(R.id.action_nav_home_to_nav_produk, null));
+        cardToko.setOnClickListener(Navigation.createNavigateOnClickListener(R.id.action_nav_home_to_nav_toko, null));
+
+        cardManage.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(getActivity(), ManageActivity.class);
+                startActivity(intent);
+            }
+        });
+
     }
 }
--- app/src/main/java/com/q/bakeryapp/ui/produk/ProdukFragment.java
+++ app/src/main/java/com/q/bakeryapp/ui/produk/ProdukFragment.java
@@ -1,22 +1,35 @@
 package com.q.bakeryapp.ui.produk;
 
 import android.os.Bundle;
-
-import androidx.fragment.app.Fragment;
-
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.cardview.widget.CardView;
+import androidx.fragment.app.Fragment;
+import androidx.navigation.Navigation;
 
 import com.q.bakeryapp.R;
 
 
 public class ProdukFragment extends Fragment {
+    CardView cardRoti, cardKukis;
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
         return inflater.inflate(R.layout.fragment_produk, container, false);
+    }
+
+    @Override
+    public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+        cardRoti = view.findViewById(R.id.cardRoti);
+        cardKukis = view.findViewById(R.id.cardKukis);
+        cardRoti.setOnClickListener(Navigation.createNavigateOnClickListener(R.id.action_nav_produk_to_nav_basah, null));
+        cardKukis.setOnClickListener(Navigation.createNavigateOnClickListener(R.id.action_nav_produk_to_nav_kering, null));
     }
 }
