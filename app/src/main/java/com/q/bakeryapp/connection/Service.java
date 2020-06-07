79c209f168c748e8235f5d851859922b52ada322
Qoolqas <qoolqas@gmail.com>
2020-06-07 08:52:28 +0700

    Delete

--- app/src/main/java/com/q/bakeryapp/connection/Service.java
+++ app/src/main/java/com/q/bakeryapp/connection/Service.java
@@ -1,13 +1,17 @@
 package com.q.bakeryapp.connection;
 
+import com.q.bakeryapp.model.delete.DeleteResponse;
 import com.q.bakeryapp.model.login.LoginResponse;
 import com.q.bakeryapp.model.produk.ProdukResponse;
 
 import retrofit2.Call;
+import retrofit2.http.DELETE;
 import retrofit2.http.Field;
 import retrofit2.http.FormUrlEncoded;
 import retrofit2.http.GET;
+import retrofit2.http.Header;
 import retrofit2.http.POST;
+import retrofit2.http.Path;
 import retrofit2.http.Query;
 
 public interface Service {
@@ -24,5 +28,7 @@ public interface Service {
     Call<LoginResponse> loginRequest(@Field("email") String email,
                                      @Field("password") String password
     );
+    @DELETE("delete.php/{id}")
+    Call<DeleteResponse> delete(@Query("produk_id")String id);
 
 }
